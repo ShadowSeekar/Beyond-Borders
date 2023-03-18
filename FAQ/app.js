59f718d39224b041229c281f58995e14c08ddce3
Aditya Bhattacharyya <128212214+Overhaul91003@users.noreply.github.com>
2023-03-19 00:30:51 +0530

    Delete FAQ directory

--- FAQ/app.js
+++
@@ -1,7 +0,0 @@
-const accordian = document.getElementsByClassName('content-container')
-console.log(accordian)
-for(i=0;i<accordian.length;i++){
-    accordian[i].addEventListener('click',function(){
-        this.classList.toggle('active')
-    })
-}
